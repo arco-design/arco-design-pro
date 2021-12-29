a49570cac4a4b981376e83aa2efa5093faf3e826
yangyuhan.79 <yangyuhan.79@bytedance.com>
2021-12-29 10:27:15 +0800

    fix: chart Slider style

--- arco-design-pro-next/src/components/Chart/period-legend-line.tsx
+++ arco-design-pro-next/src/components/Chart/period-legend-line.tsx
@@ -34,7 +34,35 @@ function PeriodLine({ data, loading }: { data: any[]; loading: boolean }) {
           }}
         />
         <Legend name="name" />
-        <Slider />
+        <Slider
+          foregroundStyle={{
+            borderRadius: ' 4px',
+            fill: 'l (180) 0:rgba(206, 224, 255, 0.9) 1:rgba(146, 186, 255, 0.8)',
+            opacity: 0.3,
+          }}
+          trendCfg={{
+            data: data.map((item) => item.rate),
+            isArea: true,
+            areaStyle: {
+              fill: 'rgba(4, 135, 255, 0.15)',
+              opacity: 1,
+            },
+            backgroundStyle: {
+              fill: '#F2F3F5',
+            },
+            lineStyle: {
+              stroke: 'rgba(36, 158, 255, 0.3)',
+              lineWidth: 2,
+            },
+          }}
+          handlerStyle={{
+            fill: '#ffffff',
+            opacity: 1,
+            width: 22,
+            height: 22,
+            stroke: '#165DFF',
+          }}
+        />
       </Chart>
     </Spin>
   );
