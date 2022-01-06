0a3f865204b94b0bc6bddc9564dd0129d75b2198
pengjiyuan <pengjiyuan@bytedance.com>
2022-01-06 20:35:03 +0800

    Update after-init

--- .arco-cli/after-init.js
+++ .arco-cli/after-init.js
@@ -1,3 +1,5 @@
+const { spawnSync } = require('child_process');
+
 const logInfo = (messages) => {
   messages.forEach((m) => {
     console.log(`\x1B[32m${m}\x1B[0m`);
@@ -5,6 +7,12 @@ const logInfo = (messages) => {
 };
 
 module.exports = ({ projectName }) => {
+  const { error } = spawnSync('yarn', ['add', 'arco-design-pro']);
+
+  if (error) {
+    throw error;
+  }
+
   logInfo([
     '******************************************************************************',
     '  Read README.md for help information. Execute following command to start',
@@ -12,4 +20,4 @@ module.exports = ({ projectName }) => {
     '    $ yarn dev',
     '******************************************************************************',
   ]);
-}
+};
