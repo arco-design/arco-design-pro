b6332b7c81d6c88cc57bcf5a6e2afff24e9c7864
goumengjie <942885975@qq.com>
2022-06-28 16:06:21 +0800

    fix:修复使用PermissionWrapper组件进行权限管理时UI会闪烁的问题.

--- arco-design-pro-next/src/components/PermissionWrapper/index.tsx
+++ arco-design-pro-next/src/components/PermissionWrapper/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useMemo } from 'react';
 import { GlobalState } from '@/store';
 import { useSelector } from 'react-redux';
 import authentication, { AuthParams } from '@/utils/authentication';
@@ -11,16 +11,14 @@ const PermissionWrapper = (
   props: React.PropsWithChildren<PermissionWrapperProps>
 ) => {
   const { backup, requiredPermissions, oneOfPerm } = props;
-  const [hasPermission, setHasPermission] = useState(false);
   const userInfo = useSelector((state: GlobalState) => state.userInfo);
 
-  useEffect(() => {
-    const hasPermission = authentication(
-      { requiredPermissions, oneOfPerm },
-      userInfo.permissions
+  const hasPermission = useMemo(() => {
+    return authentication(
+        {requiredPermissions, oneOfPerm},
+        userInfo.permissions
     );
-    setHasPermission(hasPermission);
-  }, [requiredPermissions, oneOfPerm, userInfo.permissions]);
+  },[oneOfPerm, requiredPermissions, userInfo.permissions]);
 
   if (hasPermission) {
     return <>{convertReactElement(props.children)}</>;
