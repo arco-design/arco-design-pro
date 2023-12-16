0505c476002ee8bb5d22660b51a68fbae57b4261
Ricco Xie <ricco@riccox.com>
2023-12-16 14:13:11 +0800

    fix: layout main part left padding.

--- arco-design-pro-next/src/pages/layout.tsx
+++ arco-design-pro-next/src/pages/layout.tsx
@@ -99,7 +99,7 @@ function PageLayout({ children }: { children: ReactNode }) {
     setCollapsed((collapsed) => !collapsed);
   }
 
-  const paddingLeft = showMenu ? { paddingLeft: menuWidth } : {};
+  const paddingLeft = showMenu ? { paddingLeft: 0 } : {};
   const paddingTop = showNavbar ? { paddingTop: navbarHeight } : {};
   const paddingStyle = { ...paddingLeft, ...paddingTop };
 
